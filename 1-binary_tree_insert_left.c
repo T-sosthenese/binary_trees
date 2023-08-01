2314008e237dac0f4f20a983377aa656677e1a12
sosthene <sosthenetimi94@gmail.com>
2023-08-01 14:55:08 +0300

    inserting left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		node->left = parent->left;
 		parent->left->parent = node;
 		parent->left = node;
+		node->left = parent->left->left;
 	}
 	else
 	{
